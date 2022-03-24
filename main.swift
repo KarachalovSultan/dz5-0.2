149169a64a6f41f07c3bc951313f8f2e9269e8ed
KarachalovSultan <101123912+KarachalovSultan@users.noreply.github.com>
2022-03-23 22:19:35 -0700

    Add files via upload

---
+++ main.swift
@@ -0,0 +1,58 @@
+//
+//  main.swift
+//  dz5 0.2
+//
+//  Created by Sultan on 23/3/22.
+//
+
+//1
+
+//var array: [String] = []
+//print("Напишите слово")
+//var sentence = readLine()
+//array.append(sentence! )
+//var countOfWords = 0
+//
+//
+//
+//
+//
+//
+//let QuantityWords: () -> [String] = {
+//    for i in array{
+//        print("В слове \(i.count) букв")
+//     }
+//    return array
+//}
+//
+//let quantity = QuantityWords()
+//print(quantity)
+//
+
+//2
+//
+//var cursDollara = {
+//    (number: Double) in
+//
+//    return number * 99.44
+//
+//
+//}
+//
+//print(cursDollara(10))
+
+//3
+
+//var quantity:[String] = []
+//var name1 = readLine()!
+//quantity.append(name1)
+//
+//var name2 = readLine()!
+//quantity.append(name2)
+//
+//var quantityChet = 0
+//quantityChet += 1
+//
+//let QuantityStudents  = quantity.map({"судент \(i):  "  + $0})
+//
+//print(QuantityStudents)
